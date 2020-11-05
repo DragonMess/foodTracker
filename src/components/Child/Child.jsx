a30c00f741eb982c1fb97c6426d0acf9f5d6ab7e
Camilo Pineda <kmylop@hotmail.com>
2020-11-04 23:54:21 -0500

    add meals component and modify the json data

--- src/components/Child/Child.jsx
+++ src/components/Child/Child.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
-import { Form } from 'react-bootstrap';
-import EditSaveCancel from '../EditSaveCancel/EditSaveCancel';
+import Meals from '../Meals/Meals';
 import Measures from '../Measures/Measures';
 import "./Child.css"
 function Child(props) {
-  const { firstName,lastName,height,weight,chest,head,dateMeasures} = props;
+  const { firstName,lastName,patientId } = props;
 
- const [ dataEdit, setDataEdit ] = useState(false)
+ const [ measures, setMeasures ] = useState(false)
+ const [ meals, setMeals ] = useState(false)
+ const [ program, setProgram ] = useState(false)
 
   return (
     <article className="childListSection">
@@ -17,14 +18,21 @@ function Child(props) {
         </div>
       </div>
        <div className="tabOptions">
-         <div className="option1" onClick={()=>setDataEdit(!dataEdit)}>Meausures</div>
-         <div className="option2">Meals</div>
+         <div className="option1" onClick={()=>{setMeasures(!measures)
+          setMeals(false)}}>Meausures</div>
+         <div className="option2" onClick={()=>{setMeals(!meals) 
+          setMeasures(false)}} >Meals</div>
          <div className="option3">Program</div>
        </div>
-       {dataEdit ? (
-         <div>
-        <Measures></Measures>
-      </div>
+       {measures ? (
+        <Measures
+        patientId={patientId}
+        />
+       ):(<div></div>)} 
+              {meals ? (
+        <Meals
+        patientId={patientId}
+        />
        ):(<div></div>)} 
     </article>
   )
