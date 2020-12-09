55d9456bdedce565f461a7cc5354931c7585720f
Camilo Pineda <kmylop@hotmail.com>
2020-12-09 10:14:00 -0500

    builded first deploy to test with heroku

--- src/components/Foods/Foods.jsx
+++ src/components/Foods/Foods.jsx
@@ -8,6 +8,10 @@ function Foods() {
         <div className="card-title">
           <h2 className="form-title">Foods</h2>
         </div>
+          <Form.Group controlId="formGroupFoodName" >
+    <Form.Label>Food Name</Form.Label>
+    <Form.Control type="email" placeholder="Enter Food name" className="placeHolder"/>
+  </Form.Group>
   <Form.Group controlId="exampleForm.ControlSelect2">
     <Form.Label>Select a food from the list</Form.Label>
     <Form.Control as="select" multiple>
